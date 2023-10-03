d6e2a70d6738ff25d224003aca66150ca50e9ab5
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-03 11:43:07 -0700

    testing child element calling to parent

--- styling/src/App.js
+++ styling/src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import card from './card.js';
+
+const Logo = (props) => {
+  const userPic = <img scr = {logo} alt = ""/>;
+  return userPic;
+}
 
 function App() {
   return (
     <div>
+      <h1>Hello World!</h1>
+      <Logo />
+      <card/>
     </div>
   );
 }
---
+++ styling/src/card.js
@@ -0,0 +1,10 @@
+const card = (props) => {
+    return (
+        <div>
+            <h2></h2>
+            <h3></h3>
+        </div>
+    )
+}
+
+export default card;
