4becfc6f35c5897282a442f91e5e46217ecb57b1
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-04 12:54:26 -0700

    adding states into the program

--- styling/src/App.js
+++ styling/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './card.js';
 import ModeToggler from './ModeToggler';
+import Input from './input';
 
 const Logo = (props) => {
   const userPic = <img scr = {logo} alt = ""/>;
@@ -16,6 +17,7 @@ function App() {
       <Card h2="First card" h3="First card"/>
       <Card h2="Second card" h3="Second card"/>
       <Card h2="Third card" h3="Third card"/>
+      <Input/>
     </div>
   );
 }
